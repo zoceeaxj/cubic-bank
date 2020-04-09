5132d48b0d40514f10cbfca797ce01c7eca96fbd
Nagendra <javahunk2020@gmail.com>
2020-04-09 18:20:57 -0400

    JUNIT for controller

--- banking-service/src/main/java/com/rab3tech/vo/CustomerSavingVO.java
+++ banking-service/src/main/java/com/rab3tech/vo/CustomerSavingVO.java
@@ -22,7 +22,22 @@ public class CustomerSavingVO {
 	private Date doa;
 	private String appref;
 	
+	public CustomerSavingVO() {}
 	
+	public CustomerSavingVO(int csaid, String name, String email, String mobile, String location, String accType,
+			String status, String ucrid, Date doa, String appref) {
+		this.csaid = csaid;
+		this.name = name;
+		this.email = email;
+		this.mobile = mobile;
+		this.location = location;
+		this.accType = accType;
+		this.status = status;
+		this.ucrid = ucrid;
+		this.doa = doa;
+		this.appref = appref;
+	}
+
 	public String getUcrid() {
 		return ucrid;
 	}
@@ -102,6 +117,40 @@ public class CustomerSavingVO {
 	public void setAccType(String accType) {
 		this.accType = accType;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CustomerSavingVO other = (CustomerSavingVO) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+	
 	
 }
 
