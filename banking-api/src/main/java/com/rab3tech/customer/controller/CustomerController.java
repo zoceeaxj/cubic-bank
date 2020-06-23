ab05df52ecafc3c0cfd968cabc9f78a2269f12ab
Nagendra <javahunk2020@gmail.com>
2020-06-23 14:00:57 -0400

    AJAX for username and password validation

--- banking-api/src/main/java/com/rab3tech/customer/controller/CustomerController.java
+++ banking-api/src/main/java/com/rab3tech/customer/controller/CustomerController.java
@@ -1,19 +1,40 @@
 package com.rab3tech.customer.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rab3tech.customer.service.LoginService;
 import com.rab3tech.vo.ApplicationResponseVO;
-import com.rab3tech.vo.CustomerVO;
+import com.rab3tech.vo.LoginRequestVO;
+import com.rab3tech.vo.LoginVO;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/v3")
 public class CustomerController {
 	
+	@Autowired
+	private LoginService loginService;
 	
-
+	@PostMapping("/user/login")
+	public ApplicationResponseVO authUser(@RequestBody LoginRequestVO loginRequestVO) {
+		Optional<LoginVO>  optional=loginService.findUserByUsername(loginRequestVO.getUsername());
+		ApplicationResponseVO applicationResponseVO=new ApplicationResponseVO();
+		if(optional.isPresent()) {
+			applicationResponseVO.setCode(200);
+			applicationResponseVO.setStatus("success");
+			applicationResponseVO.setMessage("Userid is correct");
+		}else {
+			applicationResponseVO.setCode(400);
+			applicationResponseVO.setStatus("fail");
+			applicationResponseVO.setMessage("Userid is not correct");
+		}
+		return applicationResponseVO;
+	}
 }
