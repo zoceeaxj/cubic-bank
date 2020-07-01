57085da9b12929dadbad34e97c58936ec33c7ac0
Nagendra Kumar <javahunk100@gmail.com>
2020-07-01 23:44:44 +0530

    JUNIT test case for service layer + mocking repository layer

--- banking-dao/src/main/java/com/rab3tech/customer/dao/repository/CustomerAccountApprovedRepository.java
+++ banking-dao/src/main/java/com/rab3tech/customer/dao/repository/CustomerAccountApprovedRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.rab3tech.dao.entity.CustomerSaving;
 import com.rab3tech.dao.entity.CustomerSavingApproved;
 
 /**
--- banking-service/src/test/java/com/rab3tech/customer/service/impl/LoginServiceImplTest.java
+++ banking-service/src/test/java/com/rab3tech/customer/service/impl/LoginServiceImplTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
@@ -16,18 +18,22 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.rab3tech.customer.dao.repository.LoginRepository;
 import com.rab3tech.dao.entity.Login;
 import com.rab3tech.dao.entity.Role;
 import com.rab3tech.vo.LoginVO;
 
-
+//JUNIT RUNNER from JUNIT
 @RunWith(MockitoJUnitRunner.class)
 public class LoginServiceImplTest {
 	
 	@Mock
 	private LoginRepository loginRepository;
+	
+	@Mock
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@InjectMocks
 	private LoginServiceImpl loginServiceImpl;
@@ -37,11 +43,55 @@ public class LoginServiceImplTest {
 	public void init() {
 		 MockitoAnnotations.initMocks(this); //Initializing mocking for each test cases
 	}
+	
+ /*	@Override
+	public String updatePassCode(String emailOrUsername,String passCode) {
+		String status="notexist";
+		Optional<Login> optional=loginRepository.findByLoginid(emailOrUsername);
+		if(optional.isPresent()) {
+			//I am loading 
+			Login login=optional.get();	
+			login.setToken(passCode);
+			//at what time this passcode was updated
+			login.setLwap(new Timestamp(new Date().getTime()));
+			status=login.getName();
+		}
+		return status;
+	} */
+	
+	@Test
+	public void testUpdatePassCodeWhenUsernameExist() {
+		Login login=new Login();
+		login.setEmail("nagen@gmail.com");
+		login.setName("Mr. Mocha");
+		login.setLoginid("owowo282");
+		Optional<Login> ologin=Optional.of(login);
+		//stubbing
+		when(loginRepository.findByLoginid("nagen@gmail.com")).thenReturn(ologin);
+		
+		String result=loginServiceImpl.updatePassCode("nagen@gmail.com", "AX&#%^@S");
+		assertEquals("Mr. Mocha", result);
+	}
+	
+	
+	@Test
+	public void testUpdatePassCodeWhenUsernameNotExist() {
+
+		Optional<Login> ologin=Optional.empty();
+		when(loginRepository.findByLoginid("sweta@gmail.com")).thenReturn(ologin);
+		
+		String result=loginServiceImpl.updatePassCode("sweta@gmail.com", "AX&#%^@S");
+		assertEquals("notexist", result);
+		
+	}
+	
 
 	@Test
 	public void testFindUserByUsernameWhenExist() {
+		
 		Login login=new Login();
 		login.setEmail("nagen@gmail.com");
+		login.setName("Mr. Mocha");
 		login.setLoginid("owowo282");
 		login.setPassword("cool@1234");
 		Set<Role> rolesSet=new HashSet<>();
@@ -51,18 +101,25 @@ public class LoginServiceImplTest {
 		rolesSet.add(role);
 		login.setRoles(rolesSet);
 		Optional<Login> ologin=Optional.of(login);
+		
+		//stubbing
 		when(loginRepository.findByLoginid("nagen@gmail.com")).thenReturn(ologin);
+		
 		Optional<LoginVO>   optional=loginServiceImpl.findUserByUsername("nagen@gmail.com");
+		
 		assertTrue(optional.isPresent());
 		assertEquals("nagen@gmail.com", optional.get().getEmail());
+		assertEquals("owowo282", optional.get().getUsername());
 	}
 	
 	@Test
 	public void testFindUserByUsernameWhenNotExist() {
+		
 		Optional<Login> ologin=Optional.empty();
-		when(loginRepository.findByLoginid("nagen@gmail.com")).thenReturn(ologin);
-		Optional<LoginVO>   optional=loginServiceImpl.findUserByUsername("nagen@gmail.com");
-		assertFalse(optional.isPresent());
+		when(loginRepository.findByLoginid("sweta@gmail.com")).thenReturn(ologin);
+		
+		Optional<LoginVO>   optional=loginServiceImpl.findUserByUsername("sweta@gmail.com");
+		assertTrue(optional.isEmpty());
 	}
 
 	@Test
